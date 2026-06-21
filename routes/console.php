16864817770cb59e79694cca22abe64ce3009709
System Admin <omareldesouky161@gmail.com>
2026-06-21 23:45:56 +0300

    Add custom artisan command to safely create admin users

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('make:admin {email} {password}', function ($email, $password) {
+    User::updateOrCreate(
+        ['email' => $email],
+        [
+            'name' => 'Aqary Admin',
+            'password' => Hash::make($password),
+            'role' => 'admin',
+            'is_verified' => true
+        ]
+    );
+    $this->info("Admin user {$email} created/updated successfully!");
+})->purpose('Create or update an admin user');
